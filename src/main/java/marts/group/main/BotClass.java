576e4b6dc0f00dc0817cd8ea2a7e87cc637cc112
Nicki-afk <casperplay45@gmail.com>
2023-08-02 17:55:33 +0300

    Added telegramm methods

--- src/main/java/marts/group/main/BotClass.java
+++ src/main/java/marts/group/main/BotClass.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
 /**
  * Hello world!
  *
  */
-public class BotClass 
+public class BotClass extends TelegramLongPollingBot
 {
 
     private Properties telegramBotProperties = new Properties();
+    private String botName;
+    private String botToken;
+    
 
 
 
@@ -28,6 +34,9 @@ public class BotClass
         InputStream inputStream = classLoader.getResourceAsStream("application.properties");
         this.telegramBotProperties.load(inputStream);
         inputStream.close();
+
+        this.botName = this.telegramBotProperties.getProperty("tg.bot.name");
+        this.botToken = this.telegramBotProperties.getProperty("tg.bot.token");
         
 
         }catch(Exception e){
@@ -35,5 +44,24 @@ public class BotClass
             e.printStackTrace();
         }
 
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update arg0) {
+        
+        
+    }
+
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return botName;
     }
 }
