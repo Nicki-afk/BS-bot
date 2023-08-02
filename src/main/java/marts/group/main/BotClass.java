d83db425d6992527bde6996a6c5825ac3338f982
Nicki-afk <casperplay45@gmail.com>
2023-08-02 17:46:58 +0300

    Added the configBot() method

--- src/main/java/marts/group/main/BotClass.java
+++ src/main/java/marts/group/main/BotClass.java
@@ -1,13 +1,39 @@
 package marts.group.main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class BotClass 
 {
+
+    private Properties telegramBotProperties = new Properties();
+
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    }
+
+
+    private void configBot(){
+        try{
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("application.properties");
+        this.telegramBotProperties.load(inputStream);
+        inputStream.close();
+        
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+
     }
 }
