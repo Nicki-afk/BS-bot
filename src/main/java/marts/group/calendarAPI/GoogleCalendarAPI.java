c9a87e6a7c12ef724f3266ed95c0799e9cf76201
Nicki-afk <casperplay45@gmail.com>
2023-08-04 00:09:11 +0300

    Connect and first config google api

--- src/main/java/marts/group/calendarAPI/GoogleCalendarAPI.java
+++ src/main/java/marts/group/calendarAPI/GoogleCalendarAPI.java
@@ -1,73 +1,143 @@
 package marts.group.calendarAPI;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Properties;
+import java.util.TimeZone;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import javax.print.DocFlavor.SERVICE_FORMATTED;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
 
 public class GoogleCalendarAPI {
+
 
      /**
    * Application name.
    */
-  private static final String APPLICATION_NAME = "telegram-bot-barbershop";
+  private  String APPLICATION_NAME;
   /**
    * Global instance of the JSON factory.
    */
-  private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+  private  JsonFactory JSON_FACTORY;
   /**
    * Directory to store authorization tokens for this application.
    */
-  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  private  String TOKENS_DIRECTORY_PATH ;
 
   /**
    * Global instance of the scopes required by this quickstart.
    * If modifying these scopes, delete your previously saved tokens/ folder.
    */
-  private static final List<String> SCOPES =
-      Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-  private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+    private  List<String> SCOPES;
 
-  /**
-   * Creates an authorized Credential object.
-   *
-   * @param HTTP_TRANSPORT The network HTTP Transport.
-   * @return An authorized Credential object.
-   * @throws IOException If the credentials.json file cannot be found.
-   */
-  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-      throws IOException {
-    // Load client secrets.
-    InputStream in = GoogleCalendarAPI.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-    if (in == null) {
-      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+    private  String SERVICE_ACCOUNT_CONFIG ;
+
+    private Calendar calendarService;
+
+
+
+
+    public void configTheGoogleCalendarAPI(Properties properties){
+
+        try{
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+
+            // Устанавливем значения переменных используя свойства 
+            this.APPLICATION_NAME = properties.getProperty("app.name");
+            this.TOKENS_DIRECTORY_PATH = properties.getProperty("app.token.directory.path");
+            this.SERVICE_ACCOUNT_CONFIG = properties.getProperty("app.service.account.config.path");
+            this.JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+            // Устанавливаем приложению права для календаря 
+            this.SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
+            
+
+
+
+
+
+            GoogleCredential credential = GoogleCredential
+                .fromStream(new FileInputStream(SERVICE_ACCOUNT_CONFIG))
+                .createScoped(SCOPES);
+
+
+            // Создаем API для клиента 
+            this.calendarService = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+
+                System.out.println("Client Endpoint API successful created!");
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+
+
+        
     }
-    GoogleClientSecrets clientSecrets =
-        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-    // Build flow and trigger user authorization request.
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-        .setAccessType("offline")
-        .build();
-    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-    Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("bot");
-    //returns an authorized Credential object.
-    return credential;
-  }
+
+
+    // 1 - Имя клиента , 2 - дата записи , 3 - имя матера , 4 - лист услуг 
+    public void createAEvent(){
+
+        try{
+
+        // пример cоздание нового события
+        Event event = new Event()
+                .setSummary("Новая стрижка")
+                .setDescription("Стрижка для клиента");
+
+        // Задание времени начала и окончания события
+        Date startDate = new Date();
+        Date endDate = new Date(startDate.getTime() + 3600000); // +1 hour
+        DateTime startDateTime = new DateTime(startDate, TimeZone.getTimeZone("UTC"));
+        EventDateTime start = new EventDateTime().setDateTime(startDateTime);
+        event.setStart(start);
+        DateTime endDateTime = new DateTime(endDate, TimeZone.getTimeZone("UTC"));
+        EventDateTime end = new EventDateTime().setDateTime(endDateTime);
+        event.setEnd(end);
+
+        // Добавление события в календарь
+         String calendarId = ""; // Здесь указать уникальный адрес календаря в профиле барбера 
+         event = this.calendarService.events().insert(calendarId, event).execute();
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+        
+    }
+
+
+
+
+
+
+
+
+  
     
 }
